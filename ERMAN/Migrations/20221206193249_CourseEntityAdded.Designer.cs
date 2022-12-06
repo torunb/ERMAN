7ba17d0922bb3246475f778b94d8f31e24e3db3a
torunb <utkuboran@windowslive.com>
2022-12-06 22:35:26 +0300

    FaqFixed

--- ERMAN/Migrations/20221206193249_CourseEntityAdded.Designer.cs
+++ ERMAN/Migrations/20221206193249_CourseEntityAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ERMAN;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ERMAN.Migrations
 {
     [DbContext(typeof(ErmanDbContext))]
-    partial class ErmanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221206193249_CourseEntityAdded")]
+    partial class CourseEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
