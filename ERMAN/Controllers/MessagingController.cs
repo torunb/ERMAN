4de1dc4f15fac6bb3a073e0919c39dc097018204
a14u <aliemre2002@gmail.com>
2022-12-07 13:45:24 +0300

    messaging almost done but not tested, however the project build fine with no errors

--- ERMAN/Controllers/MessagingController.cs
+++ ERMAN/Controllers/MessagingController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ERMAN.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NuGet.Protocol;
 using System.Net.WebSockets;
 
 namespace ERMAN.Controllers
@@ -9,13 +13,21 @@ namespace ERMAN.Controllers
     [ApiExplorerSettings(IgnoreApi = true)]
     public class MessagingController : ControllerBase
     {
+        private MessagingService _messagingService;
+
+        public MessagingController(MessagingService messagingService) {
+            _messagingService = messagingService;
+        }
+
         [Route("/ws")]
+        [Authorize(Roles = "student")]
         public async Task Get()
         {
             if (HttpContext.WebSockets.IsWebSocketRequest)
             {
                 using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-                await Echo(webSocket);
+                var userId = HttpContext.User.Claims.FirstOrDefault(claim => claim.Type == "userID");
+                await Echo(webSocket, Convert.ToInt32(userId.Value));
             }
             else
             {
@@ -23,28 +35,37 @@ namespace ERMAN.Controllers
             }
         }
 
-        private static async Task Echo(WebSocket webSocket)
+        private class ConnectionRequest {
+            public String version;
+        }
+
+        private class MessageRequest
         {
+            public String message;
+            public int to; // id of the user to send to
+        }
+
+
+        private async Task Echo(WebSocket webSocket, int userId)
+        {
+            var listener = new NotificationListener(webSocket);
+            _messagingService.register(listener, userId);
             var buffer = new byte[1024 * 4];
             var receiveResult = await webSocket.ReceiveAsync(
                 new ArraySegment<byte>(buffer), CancellationToken.None);
 
+            var connectionMessage = JsonConvert.DeserializeObject<ConnectionRequest>(receiveResult.ToJson());
+
             while (!receiveResult.CloseStatus.HasValue)
             {
-                await webSocket.SendAsync(
-                    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
-                    receiveResult.MessageType,
-                    receiveResult.EndOfMessage,
-                    CancellationToken.None);
-
                 receiveResult = await webSocket.ReceiveAsync(
                     new ArraySegment<byte>(buffer), CancellationToken.None);
-            }
 
-            await webSocket.CloseAsync(
-                receiveResult.CloseStatus.Value,
-                receiveResult.CloseStatusDescription,
-                CancellationToken.None);
+                var messageRequest = JsonConvert.DeserializeObject<MessageRequest>(receiveResult.ToJson());
+                // if messageRequest is null frontend sent malformed json, handle that case
+
+                _messagingService.sendMessage(Convert.ToInt32(userId), messageRequest.to, messageRequest.message);
+            }
         }
     }
 }
---
+++ ERMAN/Services/MessagingService.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace ERMAN.Services
+{
+    public class MessagingService
+    {
+        public Dictionary<int, NotificationListener> listeners;
+
+        public MessagingService() {
+            this.listeners = new Dictionary<int, NotificationListener>();
+        }
+
+        public async Task sendMessage(int from, int to, String message) {
+            if (this.listeners.ContainsKey(to)) {
+                await this.listeners[to].notify(from.ToString(), message);
+            }
+        }
+
+        public void register(NotificationListener listener, int userId) {
+            listeners[userId] = listener;
+        }
+    }
+    public class WSMessage {
+        String from;
+        String message;
+
+        public WSMessage(String from, String message) {
+            this.from = from;
+            this.message = message;
+        }
+    }
+
+    public class NotificationListener
+    {
+        private WebSocket websocket;
+
+
+        public NotificationListener(WebSocket websocket)
+        {
+            this.websocket = websocket;
+        }
+
+        public async Task notify(String from, String message) {
+            var msg = new WSMessage(from, message);
+            var serialized = JsonConvert.SerializeObject(msg);
+            var encoded = Encoding.UTF8.GetBytes(serialized);
+            var buffer = new ArraySegment<Byte>(encoded, 0, encoded.Length);
+
+            await this.websocket.SendAsync(
+                buffer,
+                WebSocketMessageType.Text,
+                true,
+                CancellationToken.None);
+        }
+    }
+}
