84bea3a46628a3852765ffd1bf3c9dcceab91cdf
yarkinn <yarkinnsakinci@gmail.com>
2022-12-08 18:36:54 +0300

    Update StudentPlacementController.cs

--- ERMAN/Controllers/StudentPlacementController.cs
+++ ERMAN/Controllers/StudentPlacementController.cs
@@ -11,97 +11,71 @@ namespace ERMAN.Controllers
     [ApiController]
     public class StudentPlacementController : Controller
     {
-        private readonly ErmanDbContext _dbContext;
-        private readonly StudentPlacementsRepository _studentPlacementsRepository;
-        public StudentPlacementController(ErmanDbContext dbContext)
+        private readonly IGeneralInterface<StudentPlacement,StudentPlacementDto> repository;
+        public StudentPlacementController(IGeneralInterface<StudentPlacement,StudentPlacementDto> repository)
         {
-            _dbContext = dbContext;
+            this.repository = repository;
         }
-        [HttpPost(Name = "StudentPlacementsAPI")]
-        public StudentPlacement Post(StudentPlacementDto studentPlacement)
-        {
-            var studentPlacementNew = new StudentPlacement
-            {
-                StudentId = studentPlacement.StudentId,
-                StudentFirstName = studentPlacement.StudentFirstName,
-                StudentLastName = studentPlacement.StudentLastName,
-                TranscriptGradeFromFour = studentPlacement.TranscriptGradeFromFour,
-                TranscriptGradeFromHundred = studentPlacement.TranscriptGradeFromFour,
-                TranscriptGradeContribution = studentPlacement.TranscriptGradeContribution,
-                ErasmusApplicationWithGradesBehindSeUe = studentPlacement.ErasmusApplicationWithGradesBehindSeUe,
-                UESECount = studentPlacement.UESECount,
-                UECGPA = studentPlacement.UECGPA,
-                Eng101 = studentPlacement.Eng101,
-                Eng102 = studentPlacement.Eng102,
-                LanguagePoints = studentPlacement.LanguagePoints,
-                TranscriptPoints = studentPlacement.TranscriptPoints,
-                TotalPoints = studentPlacement.TotalPoints,
-                DurationPrefered = studentPlacement.DurationPrefered,
-                UniversityChoices = studentPlacement.UniversityChoices,
-                IsInWaitingList = false,
-            };
 
-            _dbContext.StudentPlacements.Add(studentPlacementNew);
-            _dbContext.SaveChanges();
-            return studentPlacementNew;
-        }
-        [HttpPut]
-        public void Put(StudentPlacement toBeUpdated)
+        [HttpPost(Name = "StudentPlacementsAPI")]
+        public void Post(StudentPlacementDto studentPlacement)
         {
-            StudentPlacement studentPlacement = _dbContext.StudentPlacements.Find(toBeUpdated.PlacementId);
-            if (studentPlacement != null)
-            {
-                studentPlacement.StudentId = studentPlacement.StudentId;
-                studentPlacement.StudentFirstName = studentPlacement.StudentFirstName;
-                studentPlacement.StudentLastName = studentPlacement.StudentLastName;
-                studentPlacement.TranscriptGradeFromFour = studentPlacement.TranscriptGradeFromFour;
-                studentPlacement.TranscriptGradeFromHundred = studentPlacement.TranscriptGradeFromFour;
-                studentPlacement.TranscriptGradeContribution = studentPlacement.TranscriptGradeContribution;
-                studentPlacement.ErasmusApplicationWithGradesBehindSeUe = studentPlacement.ErasmusApplicationWithGradesBehindSeUe;
-                studentPlacement.UESECount = studentPlacement.UESECount;
-                studentPlacement.UECGPA = studentPlacement.UECGPA;
-                studentPlacement.Eng101 = studentPlacement.Eng101;
-                studentPlacement.Eng102 = studentPlacement.Eng102;
-                studentPlacement.LanguagePoints = studentPlacement.LanguagePoints;
-                studentPlacement.TranscriptPoints = studentPlacement.TranscriptPoints;
-                studentPlacement.TotalPoints = studentPlacement.TotalPoints;
-                studentPlacement.DurationPrefered = studentPlacement.DurationPrefered;
-                studentPlacement.UniversityChoices = studentPlacement.UniversityChoices;
-                studentPlacement.IsInWaitingList = studentPlacement.IsInWaitingList;
-                _dbContext.StudentPlacements.Update(studentPlacement);
-                _dbContext.SaveChanges();
-            }
+            repository.Add(studentPlacement);
         }
+
+        //[HttpPut]
+        //public void Put(StudentPlacement toBeUpdated)
+        //{
+        //    StudentPlacement studentPlacement = _dbContext.StudentPlacements.Find(toBeUpdated.PlacementId);
+        //    if (studentPlacement != null)
+        //    {
+        //        studentPlacement.StudentId = studentPlacement.StudentId;
+        //        studentPlacement.StudentFirstName = studentPlacement.StudentFirstName;
+        //        studentPlacement.StudentLastName = studentPlacement.StudentLastName;
+        //        studentPlacement.TranscriptGradeFromFour = studentPlacement.TranscriptGradeFromFour;
+        //        studentPlacement.TranscriptGradeFromHundred = studentPlacement.TranscriptGradeFromFour;
+        //        studentPlacement.TranscriptGradeContribution = studentPlacement.TranscriptGradeContribution;
+        //        studentPlacement.ErasmusApplicationWithGradesBehindSeUe = studentPlacement.ErasmusApplicationWithGradesBehindSeUe;
+        //        studentPlacement.UESECount = studentPlacement.UESECount;
+        //        studentPlacement.UECGPA = studentPlacement.UECGPA;
+        //        studentPlacement.Eng101 = studentPlacement.Eng101;
+        //        studentPlacement.Eng102 = studentPlacement.Eng102;
+        //        studentPlacement.LanguagePoints = studentPlacement.LanguagePoints;
+        //        studentPlacement.TranscriptPoints = studentPlacement.TranscriptPoints;
+        //        studentPlacement.TotalPoints = studentPlacement.TotalPoints;
+        //        studentPlacement.DurationPrefered = studentPlacement.DurationPrefered;
+        //        studentPlacement.UniversityChoices = studentPlacement.UniversityChoices;
+        //        studentPlacement.IsInWaitingList = studentPlacement.IsInWaitingList;
+        //        _dbContext.StudentPlacements.Update(studentPlacement);
+        //        _dbContext.SaveChanges();
+        //    }
+        //}
+
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            StudentPlacement toBeDeleted = _dbContext.StudentPlacements.Find(id);
-            if (toBeDeleted != null)
-            {
-                _dbContext.StudentPlacements.Remove(toBeDeleted);
-                _dbContext.SaveChanges();
-            }
+            repository.Remove(id);
         }
-        [HttpGet]
-        public List<StudentPlacement> Get()
-        {
-            List<StudentPlacement> list = _dbContext.StudentPlacements.ToList();
-            return list;
-        }
+
+        //[HttpGet]
+        //public List<StudentPlacement> Get()
+        //{
+        //    List<StudentPlacement> list = _dbContext.StudentPlacements.ToList();
+        //    return list;
+        //}
 
         [HttpGet("{id}")]
         public StudentPlacement Get(int id) // may return null, don't give a false id as parameter
         {
-            StudentPlacement studentPlacement = _dbContext.StudentPlacements.Find(id);
-            return studentPlacement;
+            return repository.Get(id);
         }
 
 
-        [HttpGet("waitingList")]
-        public List<StudentPlacement> GetWaitingList() // may return null, don't give a false id as parameter
-        {
-            List<StudentPlacement> list = _studentPlacementsRepository.ToWaitingList().ToList();
-            return list;
-        }
+        //[HttpGet("waitingList")]
+        //public List<StudentPlacement> GetWaitingList() // may return null, don't give a false id as parameter
+        //{
+        //    List<StudentPlacement> list = _studentPlacementsRepository.ToWaitingList().ToList();
+        //    return list;
+        //}
     }
 }
