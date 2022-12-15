2dc53a7500b3e1570f7eab8e9261bd56cd9c56d9
a14u <aliemre2002@gmail.com>
2022-12-15 16:57:37 +0300

    add authentication to file uploads/file viewing

--- ERMAN/Controllers/UploadController.cs
+++ ERMAN/Controllers/UploadController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace ERMAN.Controllers
@@ -8,14 +9,16 @@ namespace ERMAN.Controllers
     public class UploadController : Controller
     {
         [HttpPost, DisableRequestSizeLimit]
+        [Authorize(Roles = "Student, Coordinator")]
         public async Task<IActionResult> Upload(IFormFile file)
         {
+            var userId = ((int)HttpContext.Items["userId"]).ToString();
             string path = "";
             try
             {
                 if (file.Length > 0)
                 {
-                    path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "UploadedFiles"));
+                    path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "UploadedFiles/" + userId));
                     if (!Directory.Exists(path))
                     {
                         Directory.CreateDirectory(path);
@@ -42,14 +45,15 @@ namespace ERMAN.Controllers
         }
 
         [HttpPost("/api/Upload/download", Name = "DownloadFile")]
+        [Authorize(Roles = "Student, Coordinator")]
         public IActionResult Download(DownloadRequest req)
         {
-            Console.WriteLine("req filename is: " + req.fileName);
+            var userId = ((int)HttpContext.Items["userId"]).ToString();
 
             string path = "";
             try
             {    
-                path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "UploadedFiles/" + req.fileName));
+                path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "UploadedFiles/" + userId + "/" + req.fileName));
                 if (System.IO.File.Exists(path))
                 {
                     var fileContent = System.IO.File.ReadAllBytes(path);
